75054388b621cdf7cd7513af9e1815f23e4720a5
Anas El boudali <arrmani88@1337.(none)>
2020-12-07 23:24:31 +0100

    Update

--- visualizer/t.py
+++ visualizer/t.py
@@ -9,7 +9,7 @@ df = pd.DataFrame({ 'from':['A', 'B', 'C','A'], 'to':['D', 'A', 'E','C']})
 df
  
 # Build your graph
-G=nx.from_pandas_dataframe(df, 'from', 'to')
+G=nx.from_pandas_edgelist(df, 'from', 'to')
  
 # Graph with Custom nodes:
 nx.draw(G, with_labels=True, node_size=1500, node_color="skyblue", node_shape="s", alpha=0.5, linewidths=40)
