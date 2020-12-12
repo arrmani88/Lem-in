7321fb78ccc272ef2dd626e62f7cb6d4af7456b5
Anas <arrmani88@1337.(none)>
2020-12-12 16:17:22 +0100

    visualizer update

--- visualizer/set_img.py
+++ visualizer/set_img.py
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    add_icon.py                                        :+:      :+:    :+:    #
+#    set_img.py                                         :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: anel-bou <anel-bou@student.1337.ma>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/09 14:42:39 by anel-bou          #+#    #+#              #
-#    Updated: 2020/12/10 18:33:25 by anel-bou         ###   ########.fr        #
+#    Updated: 2020/12/12 16:13:48 by anel-bou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,17 +15,21 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mtimg
 from matplotlib.pyplot import imread
 
-def     add_icon(g, nodes):
-    img = imread("visualizer/img/ant.png")
-    ax=plt.gca()
-    fig=plt.gcf()
-    transax = ax.transData.transform
-    transfg = fig.transFigure.inverted().transform
-    imsize = 0.1 # this is the image size
-    for n in g.nodes():
-        (x,y) = nodes[n]
-        xx,yy = transax((x,y))
-        xa,ya = transfg((xx,yy))
-        a = plt.axes([xa-imsize/2.0,ya-imsize/2.0, imsize, imsize])
-        a.imshow(img)
-        a.axis('off')
+def	show_bg_img():
+	bgimg = imread("visualizer/img/bgimg2.jpg")
+	im_ax = plt.axes([0, 0, 1, 1])
+	im_ax.imshow(bgimg, aspect='auto')
+
+def     add_icon(g, x, y):
+	img = imread("visualizer/img/ant.png")
+	ax=plt.gca()
+	fig=plt.gcf()
+	transax = ax.transData.transform
+	transfg = fig.transFigure.inverted().transform
+	imsize = 0.1 # this is the image size
+
+	xx,yy = transax((x,y))
+	xa,ya = transfg((xx,yy))
+	a = plt.axes([xa-imsize/2.0,ya-imsize/2.0, imsize, imsize])
+	a.imshow(img)
+	a.axis('off')
--- visualizer/vi.py
+++ visualizer/vi.py
@@ -6,7 +6,7 @@
 #    By: anel-bou <anel-bou@student.1337.ma>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/06 14:42:43 by anel-bou          #+#    #+#              #
-#    Updated: 2020/12/12 14:04:49 by anel-bou         ###   ########.fr        #
+#    Updated: 2020/12/12 16:13:01 by anel-bou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imread
 from class_ant import *
-from add_icon import *
+from set_img import *
 from data_parse import *
 
 edges = []
@@ -24,18 +24,14 @@ ants.append(0)
 fd = open("out", "r")
 lines = fd.readlines()
 parse_data(lines, nodes, edges, ants)
-
 g = nx.Graph()
 plt.figure(figsize=(20, 11.25))
-
-bgimg = imread("visualizer/img/bgimg2.jpg")
-im_ax = plt.axes([0, 0, 1, 1])
-im_ax.imshow(bgimg, aspect='auto')
+show_bg_img()
 g_ax = plt.axes([0.035, 0.22, 0.92, 0.65])
 add_nodes_edges(g, edges)
-nx.draw(g, pos=nodes, ax=g_ax, with_labels=True, node_size=500, node_color="#B78356",
-node_shape="8", linewidths=30, font_size=20, font_color="#33190a",
-font_weight="bold", width=5, edge_color="#33190a")
+nx.draw(g, pos=nodes, ax=g_ax, with_labels=True, node_size=500,
+node_color="#B78356", node_shape="8", linewidths=30, font_size=20,
+font_color="#33190a", font_weight="bold", width=5, edge_color="#33190a")
 
+add_icon(g, 2, 2)
 plt.show()
-#G.add_node(4, s="v", b=5) #s= specific shape for each node
