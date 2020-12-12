34e3e9e620817a067b6afc221d38f719518c27d4
Anas <arrmani88@1337.(none)>
2020-12-12 14:08:30 +0100

    Visualizer update

---
+++ visualizer/data_parse.py
@@ -0,0 +1,47 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    data_parse.py                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: anel-bou <anel-bou@student.1337.ma>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/12/12 13:55:55 by anel-bou          #+#    #+#              #
+#    Updated: 2020/12/12 14:04:36 by anel-bou         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from class_ant import *
+
+def     room_or_link(line):
+	for c in line:
+		if c == 'L':
+			return 'A'
+		elif c == ' ':
+			return 'r'
+		elif c == '-':
+			return 'l'
+
+def     append_edge(line, edges):
+	tmp = line.split('-')
+	edges.append((tmp[0], tmp[1]))
+
+def		append_node(line, nodes):
+	tmp = line.split(' ')
+	nodes[tmp[0]] = (int(tmp[1]), int(tmp[2]))
+
+def		add_nodes_edges(g, edges):
+	g.add_edges_from(edges)
+
+def	parse_data(lines, nodes, edges, ants):
+	phase = 0
+	for line in lines:
+		if line[0] != '#':
+			var = room_or_link(line)
+			if var == 'r':
+				append_node(line[0:-1], nodes)
+			elif var == 'l':
+				append_edge(line[0:-1], edges)
+			elif var == 'A':
+				phase += 1
+				set_ant(line[0:-1], ants, phase)
+	
--- visualizer/vi.py
+++ visualizer/vi.py
@@ -6,7 +6,7 @@
 #    By: anel-bou <anel-bou@student.1337.ma>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/06 14:42:43 by anel-bou          #+#    #+#              #
-#    Updated: 2020/12/12 01:15:57 by anel-bou         ###   ########.fr        #
+#    Updated: 2020/12/12 14:04:49 by anel-bou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,45 +15,15 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import imread
 from class_ant import *
 from add_icon import *
-
-def     room_or_link(line):
-	for c in line:
-		if c == 'L':
-			return 'A'
-		elif c == ' ':
-			return 'r'
-		elif c == '-':
-			return 'l'
-
-def     append_edge(line, edges):
-	tmp = line.split('-')
-	edges.append((tmp[0], tmp[1]))
-
-def		append_node(line, nodes):
-	tmp = line.split(' ')
-	nodes[tmp[0]] = (int(tmp[1]), int(tmp[2]))
-
-def		add_nodes_edges(g):
-	g.add_edges_from(edges)
+from data_parse import *
 
 edges = []
 nodes = {}
 ants = []
 ants.append(0)
-phase = 0
 fd = open("out", "r")
 lines = fd.readlines()
-for line in lines:
-	if line[0] != '#':
-		var = room_or_link(line)
-		if var == 'r':
-			append_node(line[0:-1], nodes)
-		elif var == 'l':
-			append_edge(line[0:-1], edges)
-		elif var == 'A':
-			phase += 1
-			set_ant(line[0:-1], ants, phase)
-#1920px, 1080px, 96x96dpi = 20inc, 11.25inc
+parse_data(lines, nodes, edges, ants)
 
 g = nx.Graph()
 plt.figure(figsize=(20, 11.25))
@@ -62,7 +32,7 @@ bgimg = imread("visualizer/img/bgimg2.jpg")
 im_ax = plt.axes([0, 0, 1, 1])
 im_ax.imshow(bgimg, aspect='auto')
 g_ax = plt.axes([0.035, 0.22, 0.92, 0.65])
-add_nodes_edges(g)
+add_nodes_edges(g, edges)
 nx.draw(g, pos=nodes, ax=g_ax, with_labels=True, node_size=500, node_color="#B78356",
 node_shape="8", linewidths=30, font_size=20, font_color="#33190a",
 font_weight="bold", width=5, edge_color="#33190a")
